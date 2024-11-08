331f1918a80ac857fe1c7a01a123f7a096ec2382
Youzhou Zhang <czhang@lcs.on.ca>
2024-11-07 20:04:28 -0500

    Initial Commit

---
+++ LineAndBarExercise/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LineAndBarExercise
+//
+//  Created by Yuzhou Zhang on 2024-11-07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LineAndBarExercise/LineAndBarExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  LineAndBarExerciseApp.swift
+//  LineAndBarExercise
+//
+//  Created by Yuzhou Zhang on 2024-11-07.
+//
+
+import SwiftUI
+
+@main
+struct LineAndBarExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

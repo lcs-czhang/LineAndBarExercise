17cf98657b6bc1177bce0f4a3175d583bf69ba60
Youzhou Zhang <czhang@lcs.on.ca>
2024-11-08 19:50:14 -0500

    finished construction of a graph

--- LineAndBarExercise/ContentView.swift
+++ LineAndBarExercise/ContentView.swift
@@ -6,19 +6,41 @@
 //
 
 import SwiftUI
+import Charts
 
-struct ContentView: View {
+struct SanitizerLineGraphView: View {
+    let chartData = [
+        (sanitizer: "Common Sanitizer", data: commonSanitizerBacteriaSurvivalRate),
+        (sanitizer: "Super Sanitizer", data: superSanitizerBacteriaSurvivalRate)
+    ]
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Chart {
+                ForEach(chartData, id: \.sanitizer) { series in
+                    ForEach(series.data) { item in
+                        LineMark(
+                            x: .value("time", item.timeInSeconds),
+                            y: .value("SurvivalRate", item.bacteriaSurvivalRate)
+                        )
+                        .interpolationMethod(.monotone)
+                    }
+                    .foregroundStyle(by: .value("Sanitizer", series.sanitizer))
+                    .symbol(by: .value("Sanitizer", series.sanitizer))
+                }
+            }
+            .chartYAxis {
+                AxisMarks(position: .leading)
+            }
+            .chartPlotStyle { plotArea in
+                plotArea
+                    .background(.blue.opacity(0.1))
+            }
+            .frame(height: 300)
+            .padding()
         }
-        .padding()
     }
 }
 
 #Preview {
-    ContentView()
+    SanitizerLineGraphView()
 }
--- LineAndBarExercise/LineAndBarExerciseApp.swift
+++ LineAndBarExercise/LineAndBarExerciseApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct LineAndBarExerciseApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            SanitizerLineGraphView()
         }
     }
 }
